7a3dcebfa7b2c027e7b493df7cb108e551098626
kimohgeun <kog1059@gmail.com>
2019-02-05 16:04:21 +0900

    Setting up

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import GlobalStyle from './GlobalStyle';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<GlobalStyle />
+        모멘텀
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/Components/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    a{
+        text-decoration: none;
+        color:inherit;
+    }
+    *{
+        box-sizing:border-box;
+    }
+    body{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 1rem;
+        width:100%;
+        height:100vh;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+    }
+`;
+
+export default globalStyles;
