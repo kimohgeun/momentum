23c9005807d9139473d253b453dc794e36a1a4b7
kimohgeun <kog1059@gmail.com>
2019-03-20 19:48:53 +0900

    Updates

---
+++ precache-manifest.a3fd51ad2314744944fc9aedfdf00f30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "affc7222e5d2f04dd130",
+    "url": "/momentum/static/js/runtime~main.affc7222.js"
+  },
+  {
+    "revision": "c41043de2cd34bdd002c",
+    "url": "/momentum/static/js/main.c41043de.chunk.js"
+  },
+  {
+    "revision": "7ae9e1d6af0697fbad3e",
+    "url": "/momentum/static/js/1.7ae9e1d6.chunk.js"
+  },
+  {
+    "revision": "77c763c47f31777c3469e3548aab862e",
+    "url": "/momentum/index.html"
+  }
+];
